533078c6bdac5f2d06e9b783efd783c9b1ef3cc7
unkomorasi01 <soganokasegi0825@gmail.com>
2021-09-24 18:32:08 +0900

    スワイプ処理追加

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,47 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
+class Calculation {
+  // 2 -> 16
+  String ConvertBinaryToHex(binary) {
+    String removeZero = binary.replaceAll(RegExp(r"^0*"), "");
+    int num = int.parse(removeZero);
+    for (int i=0;i<removeZero.length;i++) {
+    }
+    return binary;
+  }
+  // 2 -> 10
+  String ConvertBinaryToDecimal(binary) {
+    return binary;
+  }
+
+  //10 -> 2
+  String ConvertDecimalToBinary(number) {
+    int num = int.parse(number);
+    return num.toRadixString(2);
+  }
+
+  //10->16
+  String ConvertDecimalToHex(number) {
+    int num = int.parse(number);
+    return num.toRadixString(2);
+  }
+
+  // 16 -> 2
+  String ConvertHexToBinary(hex) {
+    int num = int.parse(hex);
+    return num.toRadixString(2);
+  }
+  // 16 -> 10
+  String ConvertHexToDecimal(hex) {
+    int num = int.parse(hex);
+    return num.toRadixString(2);
+  }
+}
+
 class _MyHomePageState extends State<MyHomePage> {
   String _number = "";
+  String _convertResult = "";
 
   Text buttonStyle(String num) {
     return Text(
@@ -71,18 +110,46 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _clearNum() {
+    setState(() {
+      _number = "";
+    });
+  }
+
+  void _setResult(String result) {
+    setState(() {
+      _convertResult = result;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    Calculation calculation = new Calculation();
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
       body: Dismissible(
         key: Key('key'),
-        direction: DismissDirection.up,
+        direction: DismissDirection.vertical,
+        confirmDismiss: (direction) async {
+          if (direction == DismissDirection.up) {
+            _clearNum();
+          } else {
+            _setResult(calculation.ConvertBinaryToHex("$_number"));
+          }
+          return; 
+        },
         child: Column(
           children: <Widget>[
-            Padding(padding: EdgeInsets.only(top: 150)),
+            Container(
+              height: 100,
+              color: Colors.orange,
+              child: Text(
+                "$_convertResult",
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(top: 70)),
             TextField(
               textAlign: TextAlign.center,
               enabled: true,
@@ -93,7 +160,6 @@ class _MyHomePageState extends State<MyHomePage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                // Padding(padding: EdgeInsets.fromLTRB(0, 200, 0, 0)),
                 Padding(padding: EdgeInsets.only(top: 250)),
                 numButton("0"),
                 Padding(padding: EdgeInsets.only(left: 90)),
