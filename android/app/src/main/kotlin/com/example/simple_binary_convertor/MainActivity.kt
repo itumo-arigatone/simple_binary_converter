a9f3f3ee2161e7e498cbe20acf3f72e5d8db3e99
unkomorasi01 <soganokasegi0825@gmail.com>
2021-10-26 01:58:21 +0900

    rename package name

--- android/app/src/main/kotlin/com/example/simple_binary_convertor/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.simple_binary_converter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/unkomorasi01/simple_binary_convertor/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.unkomorasi01.simple_binary_converter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
